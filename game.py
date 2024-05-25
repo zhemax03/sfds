29a530868be10beac4dac7134ad26d4b93975790
zhemax03 <zhemax@yandex.ru>
2024-05-26 02:22:20 +0300

    Initial commit

---
+++ game.py
@@ -0,0 +1,28 @@
+'''Игра угадай число'''
+import numpy as np
+number = np.random.randint(1,101) #загадываем число
+
+count = 0 #считаем попытки
+lst = [] #уже загаданные числа
+while True:
+    count+=1
+    predict = int(input("Введите натуральное ичсло от 1 до 100: "))
+    
+    if predict > number:
+        lst.append(predict)
+        lst.sort()
+        print("Число должно быть меньше")
+        print(f'Уже загаданные числа: {lst}')
+    elif predict < number:
+        lst.append(predict)
+        lst.sort()
+        print("Число должно быть больше")
+        print(f'Уже загаданные числа: {lst}')
+    else:
+        print(f'Загаданное число - {number}. Твое число - {predict}')
+        print(f'Всего попыток - {count}')
+        print('Поздравляю с победой!')
+        break
+    
+
+
---
+++ game2.py
@@ -0,0 +1,43 @@
+'''Игра угадай число'''
+'''Играет компьютер'''
+import numpy as np
+
+def game(number:int=1) -> int:
+    """Указываем число, которое комп должен отгадать
+
+    Args:
+        number (int, optional): загаданное число. Defaults to 1.
+
+    Returns:
+        int: одна реализация функции - одна игра компьютера с выводом кол-ва попыток
+    """
+    count=0
+    while True:
+        count+=1
+        predict = np.random.randint(1,101)
+        if number == predict:
+            break
+    return count
+
+def simul(game, size) -> int:
+    """проверяем среднее кол-во поптыток до угадывания у нашего алгоритма
+
+    Args:
+        game (_type_): функция угадывания
+        size (_type_): размер выборки
+
+    Returns:
+        int: среднее кол-во попыток в size симуляциях игры 
+    """
+    
+    lst = []
+    # np.random.seed(1) # для воспроизводимости результата
+    predict_array = np.random.randint(1,101, size=(size))
+    for number in predict_array:
+        lst.append(game(number))
+    score = round(np.mean(lst),2)
+    print(f'Наш алгоритм угадывает число в среднем за {score} попыток')
+    return score
+
+if __name__=='__main__':
+    simul(game,1000)
